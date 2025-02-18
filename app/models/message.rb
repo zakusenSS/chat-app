de6eae4b3b08c664c2271a054dc62145b5e9a7bd
zakusenSS <cycpp866@yahoo.co.jp>
2025-02-18 15:16:44 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,6 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
-
   validates :content, presence: true, unless: :was_attached?
 
   def was_attached?
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "nameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できない場合' do
+      it "nameが空では登録できない" do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
+     it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+    end
+  end
+end
+
