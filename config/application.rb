93575b3dafb1fb797f80e005d067eb56d6bc6c27
zakusenSS <cycpp866@yahoo.co.jp>
2025-02-18 11:36:20 +0900

    Active Storageの導入

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,7 @@ module ChatApp
     config.load_defaults 7.1
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-
+    config.active_storage.variant_processor = :mini_magick
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
